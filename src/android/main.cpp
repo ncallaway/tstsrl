869cb791d6024945421147c350f72d7ca32c4259
Noah Callaway <ncallaway@gmail.com>
2012-07-28 02:12:37 -0700

    Update linux to SDL2
    Adds linux makefile, cleans up android code.

--- src/android/main.cpp
+++ src/android/main.cpp
@@ -2,25 +2,27 @@
 
 int main( int argc, char* args[] )
 {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
     //Start SDL
     SDL_Init( SDL_INIT_EVERYTHING );
 
-    SDL_Window* window = NULL;
 
     window = SDL_CreateWindow("A window", 0, 0, 640, 480, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_SHOWN);
 
-    SDL_Renderer* renderer;
     renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
 
-    //while (SDL_GetTicks() < 3000);
-	SDL_Delay(5000);
-	SDL_DestroyRenderer(renderer);
+    // Draw red
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     
-    //Quit SDL
+    // Quit SDL
     SDL_Quit();
     return 0;    
 }
--- src/linux/main.cpp
+++ src/linux/main.cpp
@@ -1,16 +1,28 @@
-#include "SDL/SDL.h"
+#include "SDL.h"
 
 int main( int argc, char* args[] )
 {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
     //Start SDL
     SDL_Init( SDL_INIT_EVERYTHING );
 
-	SDL_Surface* screen = SDL_SetVideoMode(640, 480, 0, 0);
 
-	while (SDL_GetTicks() < 3000);
+    window = SDL_CreateWindow("A window", 0, 0, 640, 480, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_SHOWN);
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    // Draw red
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     
-    //Quit SDL
+    // Quit SDL
     SDL_Quit();
-    
     return 0;    
 }
