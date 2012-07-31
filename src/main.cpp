b880a4c5d0d4337ffa0b12d39c2b527763f2543b
Noah Callaway <ncallaway@gmail.com>
2012-07-30 20:23:01 -0700

    cmake windows

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,12 @@ int main( int argc, char* args[] )
     //Start SDL
     SDL_Init( SDL_INIT_EVERYTHING );
 
-
-    window = SDL_CreateWindow("A window", 0, 0, 640, 480, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("A window", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 
     renderer = SDL_CreateRenderer(window, -1, 0);
 
-    // Draw blue 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    // Draw green 
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
