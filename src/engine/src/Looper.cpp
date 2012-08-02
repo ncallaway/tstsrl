455aac17179b96063643b827452d0642178540a1
Noah Callaway <ncallaway@gmail.com>
2012-08-01 19:52:31 -0700

    Engine library for windows

--- src/engine/src/Looper.cpp
+++ src/engine/src/Looper.cpp
@@ -1,7 +1,7 @@
 #include "Looper.h"
 
 void Looper::loop(SDL_Renderer* pRenderer) {
-    while (SDL_GetTicks() < 20000) {
+    while (SDL_GetTicks() < 2000) {
         SDL_RenderClear(pRenderer);
         SDL_RenderPresent(pRenderer);
     }
